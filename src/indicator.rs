338f61174319891f84d42e884d948f2297b60634
KmolYuan <pyslvs@gmail.com>
2021-11-01 16:17:51 +0800

    Adjust indicator functions.

--- src/indicator.rs
+++ src/indicator.rs
@@ -7,15 +7,15 @@ use alloc::{format, string::String};
 /// use yaml_peg::indicated_msg;
 ///
 /// let doc = indicated_msg("{\"a\": \n[\"b\", \"c\", \"d\"]}", 13);
-/// assert_eq!(doc, "(2:7)\n[\"b\", \"c\", \"d\"]}\n~~~~~~^")
+/// assert_eq!(doc, "2:7\n[\"b\", \"c\", \"d\"]}\n      ^")
 /// ```
 ///
 /// If print the string, it would be like:
 ///
 /// ```bash
-/// (2:7)
+/// 2:7
 /// ["b", "c", "d"]}
-/// ~~~~~~^
+///       ^
 /// ```
 ///
 /// This may be what you need if you went to indicate an error on the invalid data.
@@ -26,11 +26,11 @@ pub fn indicated_msg(doc: &str, mut pos: u64) -> String {
         if full_line as u64 > pos {
             let column = pos as usize;
             show_line = format!(
-                "({}:{})\n{}\n{}^",
+                "{}:{}\n{}\n{}^",
                 line + 1,
                 column + 1,
                 str_line,
-                "~".repeat(column)
+                " ".repeat(column)
             );
             break;
         } else {
@@ -38,4 +38,24 @@ pub fn indicated_msg(doc: &str, mut pos: u64) -> String {
         }
     }
     show_line
+}
+
+/// Same as [`indicated_msg`], but join the path before message.
+///
+/// ```
+/// use yaml_peg::indicated_msg_file;
+///
+/// let doc = indicated_msg_file("my/file.yaml", "{\"a\": \n[\"b\", \"c\", \"d\"]}", 13);
+/// assert_eq!(doc, "my/file.yaml:2:7\n[\"b\", \"c\", \"d\"]}\n      ^")
+/// ```
+///
+/// If print the string, it would be like:
+///
+/// ```bash
+/// my/file.yaml:2:7
+/// ["b", "c", "d"]}
+///       ^
+/// ```
+pub fn indicated_msg_file(path: &str, doc: &str, pos: u64) -> String {
+    format!("{}:{}", path, indicated_msg(doc, pos))
 }
